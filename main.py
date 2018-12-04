f8e4bdc3e67617ce202a1cdec08c2449022c511d
TouchTheFish <huangchaoqin1999@qq.com>
2018-12-04 19:22:18 +0800

    测试函数，用来测试其他函数

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from change_jpg_to_only012_png import change_jpg_to_only012_png
 from divide_big_to_small import divide_big_to_small
 from impulse_noise import impulse_noise
 if __name__ == "__main__":
-    print("Welcome to use 222 image processing!")
+    print("Welcome to use 222 image processing program!")
     # pirate = cv2.imread("C:\\Users\\huang\\Desktop\\ceshi\\00001771_detail.jpg")
     # pirate = cv2.cvtColor(pirate, cv2.COLOR_BGR2RGB)
     # pirate = impulse_noise(pirate, 0.06)
